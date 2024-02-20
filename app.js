effc709025b4556291a75d8eedaf415b05a5598e
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-20 15:26:30 -0500

    Intro to JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('Sup, World?');
+
+const btn = document.querySelector('button');
+
+btn.onclick = () => {
+    alert('You clicked me');
+}
+
+const fun = () => 'functional programming FTW';
+console.log(fun());
+
+class Humanoid {
+    constructor() {
+        this.dna = '🧬'
+    }
+
+    walk() {
+        console.log('going for a walk...');
+    }
+};
+
+const wait = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('thank you for waiting ⏰');
+    })
+});
